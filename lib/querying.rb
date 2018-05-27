ee283433cc101ebbc8f97777187b6a6c619d048f
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-27 22:43:26 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT COUNT(species.name) FROM species;"
+  "SELECT COUNT(characters.species) FROM characters;"
 end
 
 def select_name_and_series_subgenres_of_authors
