5e4410916f94fbfef3ce8277e8ad856495dd3608
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-27 23:06:16 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, series.title
+  "SELECT authors.name, sub_genres.name
   FROM series JOIN authors
   ON authors.id = series.author_id
   GROUP BY author_id;"
