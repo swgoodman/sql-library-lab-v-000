441f9ec1ee4b912ccdc5eaf9e824ccc725f74a5d
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-27 23:12:26 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,8 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   JOIN characters ON series.id = characters.series_id
-  ORDER BY COUNT(characters.series_id)"
+  GROUP BY series.title
+  ORDER BY COUNT(characters.series_id) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
