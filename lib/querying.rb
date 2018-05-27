60806ba73657a6863b90edb9e3e2442443d39c08
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-27 23:16:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,6 +36,7 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.book_id)
   FROM character_books
   JOIN characters ON character_books.character_id = characters.id
-  GROUP BY character_books.character_id;
+  GROUP BY character_books.character_id
+  ORDER BY COUNT(character_books.book_id);
   "
 end
