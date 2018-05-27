a71e1be3fa88d7c278725bbb828dc080c6b0c815
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-27 23:07:47 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM series JOIN authors JOIN subgenres
-  ON authors.id = series.author_id
+  ON authors.id = series.author_id, sugenres.id = series.subgenre_id
   GROUP BY author_id;"
 end
 
